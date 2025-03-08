786a6f08a650c0be29e000103f4a43a7c5629077
Erish Prajapati <irishmjn@gmail.com>
2025-03-08 19:01:10 +0545

    Visual Appealing side

--- main/settings.py
+++ main/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join('TEMPLATES')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/urls.py
+++ main/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('backend/', include("Backend.urls")),
+    path('frontend/', include("Frontend.urls")),
 ]
